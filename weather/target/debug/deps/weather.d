95f62191dea4a2265127b7b86dcdb510c791a63b
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-05-23 09:46:14 +0300

    Rusting

---
+++ weather/target/debug/deps/weather.d
@@ -0,0 +1,5 @@
+C:\Users\otirm_2hwnj4i\Documents\GitHub\Wheatherin_Rust\weather\target\debug\deps\weather.exe: src\main.rs
+
+C:\Users\otirm_2hwnj4i\Documents\GitHub\Wheatherin_Rust\weather\target\debug\deps\weather.d: src\main.rs
+
+src\main.rs:
---
+++ weather/target/debug/weather.d
@@ -0,0 +1,1 @@
+C:\Users\otirm_2hwnj4i\Documents\GitHub\Wheatherin_Rust\weather\target\debug\weather.exe: C:\Users\otirm_2hwnj4i\Documents\GitHub\Wheatherin_Rust\weather\src\main.rs
