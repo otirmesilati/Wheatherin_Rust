548f93dfc1cc5fca9f03fb76d66b9186b49b1b4c
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-05-25 15:59:10 +0300

    Rusting

--- weather/src/main.rs
+++ weather/src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 use serde::Deserialize;
 use colored::*;
+use reqwest::Response;
 
 #[derive(Deserialize, Debug)]
 struct WeatherResponse
@@ -26,6 +27,24 @@ struct Main
 }
 
 // Add the wind information entity //
+#[derive(Deserialize, Debug)]
+struct Wind
+{
+    speed: f64
+}
+
+fn get_weather_info(city: &str, country_code: &str, api_key: &str) ->
+Result<WeatherResponse, reqwest::Error>{
+    let url = format!(
+        "https://api.openweathermap.org/data/2.5/weather?q={},{}&units=metric&appid={}",
+        city,
+        country_code,
+        api_key
+    );
+    let response = reqwest::blocking::get(&url)?;
+    let response_json = response.json::<WeatherResponse>()?;
+    Ok(response_json)
+}
 
 fn main() {
     println!("Hello, world!");
