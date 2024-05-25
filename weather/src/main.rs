766db0e67ebb224613666f4f67417e001ac9e38b
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-05-25 15:14:13 +0300

    MOAR RUST!

--- weather/src/main.rs
+++ weather/src/main.rs
@@ -3,7 +3,29 @@ use serde::Deserialize;
 use colored::*;
 
 #[derive(Deserialize, Debug)]
-struct WeatherResponse;
+struct WeatherResponse
+{
+    weather: Vec<Weather>,
+    main : Main,
+    wind: Wind,
+    name: String
+}
+
+#[derive(Deserialize, Debug)]
+struct Weather
+{
+    description: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Main
+{
+    temp: f64,
+    humidity: f64,
+    pressure: f64
+}
+
+// Add the wind information entity //
 
 fn main() {
     println!("Hello, world!");
