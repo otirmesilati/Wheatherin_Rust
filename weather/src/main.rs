3c99e9f0ed13c86784c2034f301a96d60542225f
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-05-25 17:25:27 +0300

    Rusting!

--- weather/src/main.rs
+++ weather/src/main.rs
@@ -46,7 +46,66 @@ Result<WeatherResponse, reqwest::Error>{
     Ok(response_json)
 }
 
+fn display_weather_info(response: &WeatherResponse) {
+    // Extracting weather information from the response
+    let description = &response.weather[0].description;
+    let temperature = response.main.temp;
+    let humidity = response.main.humidity;
+    let pressure = response.main.pressure;
+    let wind_speed = response.wind.speed;
+
+    // Formatting weather information into a string
+    let weather_text = format!(
+        "Weather in {}: {} {}
+> Temperature: {:.1}°C,
+> Humidity: {:.1}%,
+> Pressure: {:.1} hPa,
+> Wind Speed: {:.1} m/s",
+        response.name,
+        description,
+        get_temperature_emoji(temperature),
+        temperature,
+        humidity,
+        pressure,
+        wind_speed,
+    );
+
+    // Coloring the weather text based on weather conditions
+    let weather_text_colored = match description.as_str() {
+        "clear sky" => weather_text.bright_yellow(),
+        "few clouds" | "scattered clouds" | "broken clouds" => weather_text.bright_blue(),
+        "overcast clouds" | "mist" | "haze" | "smoke" | "sand" | "dust" | "fog" | "squalls" => weather_text.dimmed(),
+        "shower rain" | "rain" | "thunderstorm" | "snow" => weather_text.bright_cyan(),
+        _ => weather_text.normal(),
+    };
+}
+
+    fn get_temperature_emoji(temperature: f64) -> &'static str {
+        if temperature < 0.0 {
+            "❄️"
+        } else if temperature >= 0.0 && temperature < 10.0 {
+            "☁️"
+        } else if temperature >= 10.0 && temperature < 20.0 {
+            "⛅"
+        } else if temperature >= 20.0 && temperature < 30.0 {
+            "🌤️"
+        } else {
+            "🔥"
+        }
+    }
+
 fn main() {
-    println!("Hello, world!");
-    println!("Weather App.")
+    println!("{}", " Welcome to Weather Station!".bright_yellow());
+    loop
+    {
+        println!("{}", "Please enter the name of the city ".bright_green());
+        let mut city = String::new();
+        io::stdin().read_line(&mut city).expect("Failed to read input");
+        let city = city.trim();
+
+        println!("{}", "Please enter the country code ".bright_green());
+        let mut country_code = String::new();
+        io::stdin().read_line(&mut country_code).expect("Failed to read input");
+        let country_code = country_code.trim();
+    }
 }
